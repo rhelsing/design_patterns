8a891bb1dd11565dee8f0d3d10adc04a11dde457
Ryan Helsing <ryanhelsing@gmail.com>
2017-02-14 09:08:28 -0500

    latest

---
+++ db_workhorse.rb
@@ -0,0 +1,7 @@
+#In order to remain performant, we should minimize queries to the database. Use Includes or Joins to load all the records and sub-records you need into an array of arrays in memory.
+
+#minimize calls, and understand db optimizations, agg queries.
+
+#then work with data in memory
+
+#avoid n+1 queries
